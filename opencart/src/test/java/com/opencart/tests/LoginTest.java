b2408a3984ad17ac7fe95348188e74d20f8e4608
rohitkumar <rk767572@gmail.com>
2026-09-13 02:58:53 +0530

    feat: complete hybrid automation framework implementation

---
+++ opencart/src/test/java/com/opencart/tests/LoginTest.java
@@ -0,0 +1,18 @@
+package com.opencart.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import com.opencart.base.BaseClass;
+import com.opencart.pages.LoginPage;
+
+public class LoginTest extends BaseClass {
+
+    @Test
+    public void loginTest() {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.doLogin(prop.getProperty("email"), prop.getProperty("password"));
+        
+        boolean flag = loginPage.isMyAccountPageExists();
+        Assert.assertTrue(flag, "Login failed or My Account page not displayed!");
+    }
+}
