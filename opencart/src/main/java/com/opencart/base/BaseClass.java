99f9d3797e1b701ff4abc384a62bbbe0940306db
rohitkumar <rk767572@gmail.com>
2026-09-13 06:01:28 +0530

    "Updated automation framework structure"

--- opencart/src/main/java/com/opencart/base/BaseClass.java
+++ opencart/src/main/java/com/opencart/base/BaseClass.java
@@ -1,26 +1,29 @@
 package com.opencart.base;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-
 public class BaseClass {
     
     public static WebDriver driver;
     public static Properties prop;
 
     public BaseClass() {
-        try {
-            FileInputStream ip = new FileInputStream("./src/main/resources/config.properties");
-            prop = new Properties();
-            prop.load(ip);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    	try {
+    	    Properties prop = new Properties();
+    	    FileInputStream ip = new FileInputStream(System.getProperty("user.dir") + "/src/main/resources/config.properties");
+    	    prop.load(ip);
+    	} catch (FileNotFoundException e) {
+    	    e.printStackTrace();
+    	} catch (IOException e) {
+    	    e.printStackTrace();
+    	}
     }
 
     @BeforeMethod
